c613dfef5f7fae05c2eda862038a21c5752d3078
SurielRuano <fsuriel33@gmail.com>
2016-10-21 17:30:00 -0500

    creacion de modelo de clientes

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cliente(models.Model):
+
+	nombre_completo = models.CharField(max_length=120)
+	fecha_nacimiento = models.DateField()
+	telefono = models.CharField(max_length=12)
+	
