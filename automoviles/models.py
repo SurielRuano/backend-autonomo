2555fcb35bf8b24856c5f359d3df3a552a57ffa7
Hugo <hugo-ensc@outlook.com>
2016-10-21 16:50:40 -0500

    Cambios en CarModel

--- automoviles/models.py
+++ automoviles/models.py
@@ -7,14 +7,14 @@ from django.db import models
 class CarModel(models.Model):
 	name = models.CharField(max_length=60)
 	anio = models.DateField(auto_now=True)
-	doors_num = models.IntegerField (max_length=2);
-	cylinders_num = models.IntegerField(max_length=2)
+	doors_num = models.IntegerField ()
+	cylinders_num = models.IntegerField()
 	description_short = models.TextField() 
 	description_long = models.TextField()
-	img_front = models.ImageField(upload_to="autos/img_front" blank=True null=True)
-	img_left = models.ImageField(upload_to="autos/img_left" blank=True null=True)
-	img_right = models.ImageField(upload_to= "autos/img_right" blank=True, null=True)
-	img_back = models.ImageField(upload_to= "autos/img_back" blank=True, null=True)
+	img_front = models.ImageField(upload_to="autos/img_front", blank=True, null=True)
+	img_left = models.ImageField(upload_to="autos/img_left", blank=True, null=True)
+	img_right = models.ImageField(upload_to= "autos/img_right", blank=True, null=True)
+	img_back = models.ImageField(upload_to= "autos/img_back", blank=True, null=True)
 	car_line = models.CharField(max_length=50)
 	price = models.FloatField()
 
