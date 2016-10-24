5692f3223beda9115013bf10fd3ab1e88dc682dd
Hugo <hugo-ensc@outlook.com>
2016-10-24 13:15:10 -0500

    Todo listo para heroku

--- back_autonomo/settings.py
+++ back_autonomo/settings.py
@@ -116,6 +116,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
@@ -123,6 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
