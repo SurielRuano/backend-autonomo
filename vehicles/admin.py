8d860fc26c2975776625783558840ac31a367a28
SurielRuano <fsuriel33@gmail.com>
2016-10-27 13:17:32 -0500

    modificacion de relaciones entre tablas de vehicle

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Vehicle, Vehicle_version, trademark
+from .models import Vehicle, Vehicle_version, trademark, vehicle_color
 
 # Register your models here.
 
 
 admin.site.register(Vehicle)
 admin.site.register(Vehicle_version)
+admin.site.register(vehicle_color)
 admin.site.register(trademark)
--- vehicles/models.py
+++ vehicles/models.py
@@ -9,13 +9,16 @@ from django.conf import settings
 class trademark(models.Model):
 	name = models.CharField(max_length=60)
 	logo = models.ImageField(upload_to="assets/trademark", blank=True, null=True)
-	
-	
-
-
 
 	def __str__(self):
 		return self.name
+
+
+class vehicle_color(models.Model):
+	color = models.CharField(max_length=60)
+
+	def __str__(self):
+		return self.color
 
 
 class Vehicle_version(models.Model):
@@ -29,41 +32,31 @@ class Vehicle_version(models.Model):
 	electric_door_locks = models.BooleanField(default=False)
 	leather_seats = models.BooleanField(default=False)
 	sport_wheels = models.BooleanField(default=False)
+	hp = models.CharField(max_length=50,blank=True, null=True)
+	engine = models.CharField(max_length=7, blank=True, null=True)
+	price = models.FloatField()
+	description_long = models.TextField()
+	doors_num = models.IntegerField ()
+	cylinders_num = models.IntegerField()
 	
-
-
 
 	def __str__(self):
 		return self.version
 
 
 class Vehicle(models.Model):
-	
+
 	trademark = models.ForeignKey(trademark)	
 	vehicle_version = models.ManyToManyField(Vehicle_version)
+	vehicle_color = models.ManyToManyField(vehicle_color)
 	name = models.CharField(max_length=60)
-	anio = models.DateField(auto_now=True)
-	doors_num = models.IntegerField ()
-	cylinders_num = models.IntegerField()
-	description_short = models.TextField() 
-	description_long = models.TextField()
+	anio = models.DateField(auto_now=True)	
+	description_short = models.TextField()	
 	img_front = models.ImageField(upload_to="assets", blank=True, null=True)
 	img_left = models.ImageField(upload_to="assets", blank=True, null=True)
 	img_right = models.ImageField(upload_to= "assets", blank=True, null=True)
 	img_back = models.ImageField(upload_to= "assets", blank=True, null=True)
-	hp = models.CharField(max_length=50,blank=True, null=True)
-	engine = models.CharField(max_length=7, blank=True, null=True)
-	price = models.FloatField()
-
-
-
-
-	# def __str__(self):
-	# 	return 'Auto {}'.format(self.name)
-
-
-# Create your models here.
-
+	
 
 	def __str__(self):
 		return self.name
