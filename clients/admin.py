90f177537396c998c4569b8b8023ae5c873ff75e
Hugo <hugo-ensc@outlook.com>
2016-10-27 13:31:53 -0500

    Se genero el modelo client

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Client
 # Register your models here.
+
+
+class ClientAdmin(admin.ModelAdmin):
+	list_display = ('full_name', 'email', 'phone_num')
+	list_filter = ('full_name', 'email')
+	search_fields = ('full_name', 'email', 'rfc')
+	ordering = ['full_name']
+
+
+admin.site.register(Client, ClientAdmin)
--- clients/models.py
+++ clients/models.py
@@ -2,5 +2,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Clients(models.Model):
-	user_name = models.CharField()
+class Client(models.Model):
+	full_name = models.CharField(max_length=120)
+	address = models.TextField()
+	email = models.EmailField()
+	phone_num = models.CharField(max_length=12)
+	birthday = models.DateField()
+	rfc = models.CharField(max_length=13)
+	curp = models.CharField(max_lenght=18)
+	gender = models.CharField(max_lenght=40)
+	nationality = models.CharField(max_lenght=100)
+	marital_status = models.CharField(max_lenght=40)
+	scholarship = models.CharField(max_lenght=40)
+	ocuppation = models.CharField(max_length=200)
+	salary = models.FloatField()
+	personale_references = models.TextField()
+
+	class Meta:
+		verbose_name="Client"
+		verbose_name_plural = "Clients"
+
+	def __str__(self):
+		return 'Cliente {}'.format(self.full_name)
