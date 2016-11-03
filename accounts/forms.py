f8f2e5a96aeafb1d3e8d38af9bd189c3d1caae0b
Hugo <hugo-ensc@outlook.com>
2016-11-03 10:01:15 -0600

    Se agrego el template registro

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(forms.ModelForm):
+	password = forms.CharField(label="Password", widget=forms.PasswordInput)
+	password = forms.CharField(label="Repeat your Password", widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ('username', 'email')
+
+	def clean_password(self):
+		clean = self.cleaned_data
+		if clean ['password'] != clean['password2']:
+			raise forms.ValidationError("Passwords don't match")
+		return clean['password2']
