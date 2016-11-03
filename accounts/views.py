d95cfd8f08eba9957578ca5dce578b5c1a4bf8cf
Hugo <hugo-ensc@outlook.com>
2016-11-03 12:41:13 -0600

    Se pueden generar usuarios mediante un formulario

--- accounts/views.py
+++ accounts/views.py
@@ -11,3 +11,17 @@ class RegistryView (View):
 		'form':form,
 		}
 		return render(request, template_name, context)
+
+	def post(self,request):
+		template_name = "registration/registry.html"
+		new_user_f = UserRegistrationForm(request.POST)
+		if new_user_f.is_valid():
+			new_user = new_user_f.save(commit=False)
+			new_user.set_password(new_user_f.cleaned_data['password'])
+			new_user.save()
+			return redirect('home')
+		else:
+			context = {
+			'form':new_user_f,
+			}
+			return redirect(request, template_name, context)
