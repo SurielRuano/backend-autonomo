ed54a3fbbd6367199991447867afa3546e67ebf6
Hugo <hugo-ensc@outlook.com>
2016-11-03 16:59:00 -0600

    Profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
+	url(r'^profile/$', views.ProfileView.as_view(), name="profile"),
 	url(r'^registry/$', views.RegistryView.as_view(), name="registry"),
 	url(r'^login/$', login, name="login"),
 	url(r'^logout/$', logout, name="logout"),
