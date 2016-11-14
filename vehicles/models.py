314467464a709605157234906f1977b626a4bcc0
SurielRuano <fsuriel33@gmail.com>
2016-11-14 17:10:22 -0600

    cambio en modelo de version vehicles

--- vehicles/models.py
+++ vehicles/models.py
@@ -12,48 +12,65 @@ class trademark(models.Model):
 		return self.name
 
 
-
-
-
 class Vehicle(models.Model):
 
 	trademark = models.ForeignKey(trademark)	
 	name = models.CharField(max_length=60)
 	anio = models.DateField(auto_now=True)	
 	description_short = models.TextField()	
-	img_front = models.ImageField(upload_to="assets/vehicles")
-	img_left = models.ImageField(upload_to="assets/vehicles", blank=True, null=True)
-	img_right = models.ImageField(upload_to= "assets/vehicles", blank=True, null=True)
-	img_back = models.ImageField(upload_to= "assets/vehicles", blank=True, null=True)
+	img_front = models.ImageField(upload_to="assets/vehicles/%Y/%m/%d/", blank=True, null=True)
+	img_left = models.ImageField(upload_to="assets/vehicles/%Y/%m/%d/", blank=True, null=True)
+	img_right = models.ImageField(upload_to= "assets/vehicles/%Y/%m/%d/", blank=True, null=True)
+	img_back = models.ImageField(upload_to= "assets/vehicles/%Y/%m/%d/", blank=True, null=True)
 	
 
 	def __str__(self):
 		return self.name
 
 
-
-
 class Vehicle_version(models.Model):
+
 	vehicle = models.ForeignKey(Vehicle, related_name='version')	
-	version = models.CharField(max_length=60)
-	img_front = models.ImageField(upload_to="assets/vehicles/version")	
+	version = models.CharField(max_length=60)	
+	transmission = models.CharField(max_length=7, blank=True, null=True)
+	gas_type = models.CharField(max_length=7, blank=True, null=True)	
+	hydraulic_power_steering = models.BooleanField(default=False)	
+	hp = models.CharField(max_length=50,blank=True, null=True)
+	engine = models.CharField(max_length=7, blank=True, null=True)
+	price = models.FloatField()
+	doors_num = models.IntegerField ()
+	cylinders_num = models.IntegerField()
+	description_long = models.TextField()
+
+	#security
 	airbag = models.BooleanField(default=False)
-	sunroof = models.BooleanField(default=False)
-	bluetooth = models.BooleanField(default=False)
-	hydraulic_power_steering = models.BooleanField(default=False)
-	autoestereo = models.BooleanField(default=False)
+	brake_abs = models.BooleanField(default=False)
+	alarm = models.BooleanField(default=False)
+	stability_control = models.BooleanField(default=False)
+	foglamps = models.BooleanField(default=False)
+	#confort
 	electric_glasses = models.BooleanField(default=False)
 	electric_door_locks = models.BooleanField(default=False)
 	leather_seats = models.BooleanField(default=False)
 	sport_wheels = models.BooleanField(default=False)
-	hp = models.CharField(max_length=50,blank=True, null=True)
-	engine = models.CharField(max_length=7, blank=True, null=True)
-	price = models.FloatField()
-	description_long = models.TextField()
-	doors_num = models.IntegerField ()
-	cylinders_num = models.IntegerField()
-	
+	air_conditioner = models.BooleanField(default=False)
+	electric_mirrors = models.BooleanField(default=False)
+	rear_wiper = models.BooleanField(default=False)
+	xenon_lights = models.BooleanField(default=False)
+	sunroof = models.BooleanField(default=False)
+	autoestereo = models.BooleanField(default=False)	
+	bluetooth = models.BooleanField(default=False)
+	cd = models.BooleanField(default=False)
+	dvd = models.BooleanField(default=False)
+	usb = models.BooleanField(default=False)
+	mp3 = models.BooleanField(default=False)
+	radio = models.BooleanField(default=False)
 
+	img_v1 = models.ImageField(upload_to="assets/vehicles/version")
+	img_v2 = models.ImageField(upload_to="assets/vehicles/version")
+	img_v3 = models.ImageField(upload_to="assets/vehicles/version")
+	img_v4 = models.ImageField(upload_to="assets/vehicles/version")
+	
 
 
 	def fee_mounth_48(self):
